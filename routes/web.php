ee98103f26130ca86c748b52cc28258044101aae
Sam Heney <sheney10@gmail.com>
2020-08-26 09:33:47 +0100

    added models for Qs and As added basic routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Question;
+use App\Answer;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+ * Show question list
+ * and form for new questions
+ */
 Route::get('/', function () {
-    return view('welcome');
+    return view('questions');
+});
+
+/*
+ * Create a new question
+ */
+route::post('/question', function (Request $request) {
+    return view('/');
+});
+
+/*
+ * Show question detail, answers
+ * and form for answering
+ */
+route::get('/question/{question}', function (Question $question) {
+    return view('question');
+});
+
+/*
+ * Create a new answer
+ */
+route::post('/answer', function (Request $request) {
+    return view('question');
 });
